474099adc188cff7e6030dacf62bb1cb2f8f0462
NForlini <108763722+NForlini@users.noreply.github.com>
2023-12-04 22:27:02 -0500

    Day 1 of 100 coding
    final update

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ items = {
 player_location = rooms["Outside"][0]
 
 #directional commands
-command_list = "open, close, left, right, forward, back, up, down, take, drop, look, enter lighthouse, help"
+command_list = "open, close, left, right, forward, back, up, down, take, drop, look, unlock, leave, enter lighthouse, help"
 
 #inventory
 inv = []
@@ -64,7 +64,7 @@ while user_input != "enter lighthouse":
               "The musty smell shows the lighthouses age.\n"
               "It shows that humans haven't been here for a long time...\n")
         player_location = rooms["Floor 1"][0]
-    elif user_input == "back":
+    elif user_input == "leave":
         print("You have chosen to abandon the lottery,\n"
               "You leave but cannot return to your home...\n"
               "You have lost the game, Thank you for playing!")
@@ -92,7 +92,7 @@ while user_input != "open right door":
         player_location = rooms["Floor 2"][0]
     elif user_input == "open left door":
         print("The old handle jiggles but does not move, the lock is sturdy,\n"
-              "The only thing to unlock is a key,\n")
+              "The only thing to unlock it is a key,\n")
     else:
         print("This is not an option.")
 
@@ -177,7 +177,7 @@ while user_input != "open door" or user_input != "open left door":
         player_location = rooms["Floor 3"][1]
     else:
         print("This is not an option.")
-
+#3rd floor
 user_input = ""
 print("The door lets out into the next room,\n"
       "There is a bed with a small end-table next to it in the corner.\n"
@@ -206,10 +206,12 @@ while user_input != "open door" or user_input != "open left door":
     else:
         print("This is not an option.")
 
-#4th floor
+
 user_input = ""
-print("The door lets out into a room with table.\nOn its few plants, most dead and withered,\n"
-      "Some dried, still attached to their long dead roots nestled in all of the plants sit a morter and pestle and a few scattered sachets\n"
+print("The door lets out into a room with table.\n"
+      "On it, is few plants, most dead and withered,\n"
+      "Some dried, still attached to their long dead roots\n"
+      "Nestled in all of the plants sit a morter and pestle and a few scattered sachets of dry seaweed\n"
       "And a door on the right.\n")
 while user_input != "open door" or user_input != "open right door":
     user_input = input("What will you do? \n").lower()
@@ -226,15 +228,18 @@ while user_input != "open door" or user_input != "open right door":
     elif user_input == "dry seaweed" and "dry seaweed" in inv:
         print("You have taken the dry seaweed already! \n")
     elif user_input == "take dry seaweed" and not "dry seaweed" in inv:
-        print("You look at the table.\nYou grab the few leave you can and grind them.\n"
-              "They leave a dark green powder\nYou fill a few sachets with it\n")
+        print("You look at the table.\n"
+              "Grabbing the few leaves, you grind them.\n"
+              "They leave a dark green powder\n"
+              "You fill a few sachets with it\n")
         inv.append("dry seaweed")
     elif user_input == "open door" or "open right door":
         print("You open the door, let out into the next room.\n")
-        break######################
+        break
         player_location = rooms["Floor 4"][1]
     else:
         print("This is not an option.")
+
 
 print("You enter into the next room, their seem to be medical supplies lying around and a table.\n"
       "It is large enough to a man to lay on, what happened here?\n"
@@ -271,24 +276,80 @@ while user_input != "open door" or user_input != "open right door":
     user_input = input("what will you do? \n").lower()
     if user_input == "help":
         print(command_list)
-    elif user_input == "look" and not "key" in inv:
-        print("You have reached the top of the lighthouse.\n"
-              "You can see almost nothing in the pitch black of the night.\n"
-              "The light looks like it hasn't been lit in a very long time.\nLooking around you see a key,\n"
-              "Near the now dead light on the floor.\n")
-    elif user_input == "look" and "key" in inv:
-        print("It shows that humans haven't been here for a long time... \n"
-              "There is a door on the right.")
-    elif user_input == "key" and "key" in inv:
-        print("You have taken the key already! \n")
     elif user_input == "take key" and not "key" in inv:
-        print("You look at the table.\nYou grab the few leave you can and grind them."
-              "They leave a dark green powder\nYou fill a few sachets with it\n")
+        print("The key feels rough, bubbled from the age and rust that is eating away at it\n")
+    elif user_input == "look" and "key" in inv:
+        print("You start thinking of your decent\n")
+    elif user_input == "take key" and "key" in inv:
+        print("You have taken the key already! \n")
         inv.append("key")
     elif user_input == "go back" or "open right door":
-        print("You open the door, let out into the next room.\n")
-        player_location = rooms["Floor 4"][1]
+        print("You open the door.\n")
+        player_location = rooms["Floor 1"][0]
         break
     else:
         print("This is not an option.")
+#decent
+print("Now you begin your decent, with every step you feel the dread builing.\n"
+      "You know you will have to fight this monster.\n"
+      "How many have died before you trying to live?\n"
+      "How many have been claimed, killed, to satisfy the Nuckelavee?\n"
+      "Your thoughts consume you as you move, almost robotically to the start.\n"
+      "To the heavy lock that you know is the only thing holding your death at bay.")
+while user_input == input("what will you do? \n").lower():
+    if user_input == "help":
+        print(command_list)
+    elif user_input== "unlock" and "key" in inv:
+        print("The heavy, old, rusted padlock creaks.\n"
+              "Its years of being maintained by the cleaners, show how often the lock has been relocked\n"
+              "How many times someone has died here\n")
+        player_location = rooms["Basement"]
+    elif user_input == "unlock" and not "key" in inv:
+        print("The lock is old and rusted,\n"
+              "heavy to keep the monster at bay\n"
+              "Find the key")
+    elif user_input == "leave":
+        print("You have chosen to abandon the lottery,\n"
+              "You leave but cannot return to your home...\n"
+              "You have lost the game, Thank you for playing!")
 
+#basement
+print("The basement is dark and has an unnatural chill to it.\n"
+      "The door is open, giving a bit of light in the otherwise pitch black room.\n"
+      "You move in further\n"
+      "You hear breathing as you feel your light eclipse as the Nuckelavee moves being you.\n"
+      "Breaking out into a cold sweat you turn around to face it\n"
+      "\n"
+      "\n"
+      "Beast was a good word to describe this spirit.\n"
+      "It was a mass of flesh and tendon.\n"
+      "A cyclops horse with a human torso fused to it, riding in a perverse night hunt\n"
+      "The arms of the 'human' were unnaturally long, almost dragging on the floor\n"
+      "with long fingers that could be compared to talons"
+      "You see the veins in the muscles, they were the only reality that show this wasn't a made up being\n"
+      "\n"
+      "\x1BThat it could be killed.\x1B"
+      "\n"
+      "\n"
+      "\n"
+      "The horse gnashed and the 'human' let out an unnatural screech as it came at you.\n"
+      "Growing up in the small village, you never learned sword-play; it wasn't as useful as farming\n"
+      "The Nuckelavee knocks you down, its hot breathe reeking in your face as it uses its arms to hold you down\n"
+      "Fear grips you as you inch your hand into your pocket\n"
+      "Gripping the few sachets you put there, you pull one out, the cloak providing you enough wiggle room.\n"
+      "The Nuckelavee's face is getting closer with its mouth opening.\n"
+      "In that instant, you toss the sachet into its mouth and it bites down reactively.\n"
+      "It lets out a shriek of pain and launches away from you, the sachet burning the horse mouth\n"
+      "Taking the jar of water out of your cloak, you pour it on the sword, hoping that it will help the blade some\n"
+      "\n"
+      "\n"
+      "You run at the creature, sword drawn, throwing another sachet with your other hand.\n"
+      "The sachet breaks open, releasing a find mist of dried seaweed over the creature.\n"
+      "Closing the gap, you plunge the sword into the breast-plate of the horse-man\n"
+      "The sound is something you will never forget.\n"
+      "\n"
+      "\n"
+      "You did it. You killed the Nuckelavee\n"
+      "You saved anyone else from having to win the lottery\n"
+      "The curdling scream of the Nuckelavee would haunt you for the rest of your days\n"
+      "You have truly won the lottery...")
